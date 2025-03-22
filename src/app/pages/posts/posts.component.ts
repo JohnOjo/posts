c3fa4a2c58e87f25a767a85d1c34ecadaa9ac561
JohnOjo <u15096794@tuks.co.za>
2025-03-22 19:37:55 +0200

    Add animations

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -10,6 +10,7 @@ import {
   selectPosts,
 } from '../../store/selectors/posts.selectors';
 import { CommonModule } from '@angular/common';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-posts',
@@ -17,15 +18,32 @@ import { CommonModule } from '@angular/common';
   imports: [CardComponent, CommonModule],
   templateUrl: './posts.component.html',
   styleUrl: './posts.component.scss',
+  animations: [
+    trigger('bounce', [
+      transition('void => *', [
+        animate(
+          '{{delay}}',
+          keyframes([
+            style({ transform: 'scale(1,1) translateY(0)' }),
+            style({ transform: 'scale(1.1, 0.9) translateY(0)' }),
+            style({ transform: 'scale(0.9, 1.1) translateY(-35px)' }),
+            style({ transform: 'scale(1.05, 0.95) translateY(0)' }),
+            style({ transform: 'scale(1,1) translateY(-7px)' }),
+            style({ transform: 'scale(1,1) translateY(0)' }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class PostsComponent implements OnInit {
   private store = inject(Store);
-
   posts$: Observable<any[]> = this.store.select(selectPosts);
   loading$: Observable<boolean> = this.store.select(selectLoading);
   error$: Observable<string | null> = this.store.select(selectError);
   activePost: Post | null = null;
   activePostClicksCount: number = 0;
+  bouncAnimationDelays: number[] = [1,2,3,4]
 
   ngOnInit() {
     this.store.dispatch(loadPosts());
